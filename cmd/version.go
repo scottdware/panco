9caba9d607d233d3721548d6ac60ef6a8f913290
Scott Ware <scottdware@users.noreply.github.com>
2022-11-05 14:20:49 -0400

    Updated version

--- cmd/version.go
+++ cmd/version.go
@@ -48,7 +48,7 @@ var versionCmd = &cobra.Command{
 newer version available.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var releases []release
-		curver := "v2022.02"
+		curver := "v2022.11"
 
 		resp, err := resty.R().
 			SetHeader("Content-Type", "application/json").
